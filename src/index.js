dfa596fc9964f9e451da8516c29813a41b2c92fb
Tcaprioli <Capriolityler@gmail.com>
2020-02-21 18:33:44 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  //Array that holds input
+  let toDoList = []
+
+
+  //"$()" is short hand for "document.getElementById"
+  function $(x) {return document.getElementById(x);}
+
+
+  //returns the last element pushed into the array
+  function showListItem(listArray){
+    return bulletList(listArray[listArray.length-1])
+  }
+
+
+  //Creates li nodes/adds interaction
+  function bulletList(content){
+    let li = document.createElement("li")
+  
+    $("tasks").appendChild(li)
+    li.innerHTML = content + " <button class='delete'>Delete</button>"
+    li.querySelector(".delete").addEventListener("click", e =>  
+          li.remove()
+        )//end of eventlistener
+    
+  }
+  
+  //Adds input to to-do array
+  $("create-task-form").addEventListener('submit', function(event) {
+    event.preventDefault()
+    listItem = $("new-task-description").value
+    toDoList.push(listItem)
+    console.log(toDoList)
+    
+    return showListItem(toDoList)
+  })
+  
+  function mkeButton(buttonType){
+    let button = document.createElement("BUTTON")
+    button.createTextNode(buttonType)
+
+  }
+    
+  
+}); //DOM event listener/ end of code
